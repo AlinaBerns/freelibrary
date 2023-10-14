9e884f62898d202f7267f0fda11cab9c44851cd7
AlinaBerns <123542234+AlinaBerns@users.noreply.github.com>
2023-10-14 14:40:04 +0200

    +registration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './user/home/home.component';
 import { AccountComponent } from './user/account/account.component';
 import { CartComponent } from './user/cart/cart.component';
 import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 const routes: Routes = [
   {path: '', component:HomeComponent},
   {path: 'account', component:AccountComponent},
   {path: 'cart', component:CartComponent},
-  {path:'login', component:LoginComponent}
+  {path:'login', component:LoginComponent},
+  {path:'registration', component:RegistrationComponent}
 ];
 
 @NgModule({
--- src/app/theme.service.ts
+++ src/app/theme.service.ts
@@ -6,15 +6,21 @@ import { Injectable, Renderer2, RendererFactory2 } from '@angular/core';
 export class ThemeService {
   private renderer: Renderer2;
   private isDark: boolean;
+  private isLight: boolean;
+  private isMid: boolean;
 
   constructor(private rendererFactory: RendererFactory2) {
     this.renderer = rendererFactory.createRenderer(null, null);
     this.isDark = this.getInitialTheme();
+    this.isLight=this.getInitialTheme2();
+    this.isMid = this.getInitialTheme3();
     this.updateTheme();
   }
 
   toggleTheme() {
     this.isDark = !this.isDark;
+    this.isLight = !this.isLight;
+    this.isMid =!this.isMid;
     this.saveThemePreference();
     this.updateTheme();
   }
@@ -24,9 +30,19 @@ export class ThemeService {
     // If a theme preference is saved in localStorage, use it; otherwise, default to light.
     return savedTheme === 'dark';
   }
+  private getInitialTheme2(): boolean {
+    const savedTheme = localStorage.getItem('theme');
+    // If a theme preference is saved in localStorage, use it; otherwise, default to light.
+    return savedTheme === 'light';
+  }
+  private getInitialTheme3(): boolean {
+    const savedTheme = localStorage.getItem('theme');
+    // If a theme preference is saved in localStorage, use it; otherwise, default to light.
+    return savedTheme === 'mid';
+  }
 
   private saveThemePreference() {
-    localStorage.setItem('theme', this.isDark ? 'dark' : 'light');
+    localStorage.setItem('theme', this.isDark ? 'dark' :  'light');
   }
 
   private updateTheme() {
